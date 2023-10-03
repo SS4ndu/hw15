4690d0152b196851ef773a463a2b949b962f2b41
sandu <badanausandu@gmail.com>
2023-10-03 20:04:13 +0300

    first commit

---
+++ export.js
@@ -0,0 +1,8 @@
+import { array } from "./index.js";
+import { nume } from "./index.js";
+import { suma } from "./index.js";
+
+
+console.log(array('text', 'text2', 'text3'));
+console.log(nume('Alexandru', 'Badanau'));
+console.log(suma(5, 7));
---
+++ index.js
@@ -0,0 +1,26 @@
+// function declaration
+
+export function suma(nr1, nr2) {
+    return nr1 + nr2
+}
+
+
+
+
+// function expresion
+
+
+export var nume = function (lastName, firstName) {
+    return firstName + ' ' + lastName
+}
+
+
+
+
+export function array(numb1, numb2, numb3, numb4, numb5) {
+    return arguments
+}
+
+
+
+
